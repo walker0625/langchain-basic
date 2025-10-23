5afee91f2811c66fec7850ff28e0456fa1b7418e
walker0625 <jeonminwoo0625@gmail.com>
2025-10-23 11:58:13 +0900

    mcp advance(exa/brave/perplexity)

--- 12_mcp/mcp_multi_client.py
+++ 12_mcp/mcp_multi_client.py
@@ -5,55 +5,97 @@ from dotenv import load_dotenv
 from langchain_mcp_adapters.client import MultiServerMCPClient
 
 from langchain_openai import ChatOpenAI
-from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_core.messages import HumanMessage
 from langgraph.prebuilt import create_react_agent
 
 load_dotenv()
-SMITHERY_AI_KEY = os.getenv('SMITHERY_AI_KEY')
-SMITHERY_AI_PROFILE = os.getenv('SMITHERY_AI_PROFILE')
+SMITHERY_API_KEY = os.getenv('SMITHERY_API_KEY')
+SMITHERY_API_PROFILE = os.getenv('SMITHERY_API_PROFILE')
 
-
-from urllib.parse import urlencode
-params = urlencode({"api_key": SMITHERY_AI_KEY, "profile": SMITHERY_AI_PROFILE})
-
+# from urllib.parse import urlencode
+# params = urlencode({"api_key": SMITHERY_API_KEY, "profile": SMITHERY_API_PROFILE})
 
 # 실제 실행 파트
 async def main():
-    print("멀티 클라이언트 세팅 중...")
+    
+    print("Multi Server MCP Client Setting...")
     
     client = MultiServerMCPClient(
         {
-            # "Math" : {
-            #     "command" : "python",
-            #     "args" : ["C:/Potenup/LLM-Study/12_mcp/odd_math_server.py"],
-            #     "transport" : "stdio"
+            # "math": {
+            #     "command": "python",
+            #     "args": ["C:/Potenup/LLM-Study/12_mcp/odd_math_server.py"],
+            #     "transport": "stdio"
             # },
-            # "CustomWeather" : {
-            #     "url" : "http://localhost:8100/mcp",
-            #     "transport" : "streamable_http"
+            # "local_mcp_weather_server": {
+            #     "url": "http://localhost:8100/mcp",
+            #     "transport": "streamable_http"
             # },
-            # "mcp_weather_server": {
+            # "remote_mcp_weather_server": {
             #     "url": f"https://server.smithery.ai/@isdaniel/mcp_weather_server/mcp?{params}",
-            #     "transport" : "streamable_http"
+            #     "transport": "streamable_http"
             # },
-            "mcp_weather_server": {
-                "transport" : "stdio",
+            # "remote_mcp_weather_server": {
+            #     "transport": "stdio",
+            #     "command": "npx",
+            #     "args": [
+            #         "-y",
+            #         "@smithery/cli@latest",
+            #         "run",
+            #         "@isdaniel/mcp_weather_server",
+            #         "--key",
+            #         SMITHERY_API_KEY,
+            #         "--profile",
+            #         SMITHERY_API_PROFILE
+            #     ]
+            # },
+            "exa_search_mcp": {
+                "transport": "stdio",
                 "command": "npx",
                 "args": [
                     "-y",
                     "@smithery/cli@latest",
                     "run",
-                    "@isdaniel/mcp_weather_server",
+                    "exa",
                     "--key",
-                    SMITHERY_AI_KEY,
+                    SMITHERY_API_KEY,
                     "--profile",
-                    SMITHERY_AI_PROFILE
+                    SMITHERY_API_PROFILE
+                ]
+            },
+            "brave_search_mcp": {
+                "transport": "stdio",
+                "command": "npx",
+                "args": [
+                    "-y",
+                    "@smithery/cli@latest",
+                    "run",
+                    "@JonyanDunh/brave-search-mcp",
+                    "--key",
+                    SMITHERY_API_KEY,
+                    "--profile",
+                    SMITHERY_API_PROFILE
+                ]
+            },
+            "perplexity-search": {
+                "transport": "stdio",
+                "command": "npx",
+                "args": [
+                    "-y",
+                    "@smithery/cli@latest",
+                    "run",
+                    "@arjunkmrm/perplexity-search",
+                    "--key",
+                    SMITHERY_API_KEY,
+                    "--profile",
+                    SMITHERY_API_PROFILE
                 ]
             }
         }
     )
 
     tools = await client.get_tools()
+    
     for tool in tools:
         print(tool.name)
     
@@ -64,29 +106,22 @@ async def main():
         temperature=0
     )
     
-    agent_excutor = create_react_agent(model=model, tools=tools)
+    agent_executor = create_react_agent(model=model, tools=tools)
 
     # # 더하기 도구
-    # system_message = SystemMessage(
-    #     content="너는 주어진 도구를 사용해서 잘못된 수학 계산을 하는 MCP 에이전트야"
-    # )
-    # response = await agent_excutor.ainvoke(
+    # response = await agent_executor.ainvoke(
     #     {"messages" : [system_message, HumanMessage(content="1+2 는?")]}
     # )
     # print("답변: ", response["messages"][-1].content)
 
     # # 곱하기 도구
-    # response = await agent_excutor.ainvoke(
+    # response = await agent_executor.ainvoke(
     #     {"messages" : [system_message, HumanMessage(content="2*3 는?")]}
     # )
     # print("답변: ", response["messages"][-1].content)
 
-    # 날씨 도구
-    system_message = SystemMessage(
-        content="너는 주어진 도구를 사용해서 날씨를 알려주는 에이전트야"
-    )
-    response = await agent_excutor.ainvoke(
-        {"messages" : [HumanMessage(content="석촌동의 날씨를 알려줘")]}
+    response = await agent_executor.ainvoke(
+        {"messages" : [HumanMessage(content="삼성전자의 ai 사업 도입 현황에 대해서 조사해줘")]}
     )
     print("답변: ", response["messages"][-1].content)
 
